e6608676ef58757c6ef43d8f88d54bab67acd565
dhilipkumars <dhilip.kumar.s@huawei.com>
2016-01-03 12:09:35 +0000

    Instance code stabilied

--- common/test_store.go
+++ common/test_store.go
@@ -2,33 +2,51 @@ package main
 
 import (
 	"./id"
-	"./store"
 	"./store/etcd"
-	"fmt"
+	"./types"
+	"log"
 )
 
 func main() {
 
-	/* Create a variable for the DB interface */
-	var etcdConn store.DB
 	/* Create a variable on the implemented datastore , in this case etcd */
-	etcdConn = etcd.New()
+	types.Gdb = etcd.New()
 
 	/* Setup etcd with the etcd endpoint*/
-	err := etcdConn.Setup("http://10.11.12.24:2379")
+	err := types.Gdb.Setup("http://10.11.12.24:2379")
 	/* Test if this is setup */
-	fmt.Printf("IsSetup %v\n", etcdConn.IsSetup())
+	log.Printf("IsSetup %v\n", types.Gdb.IsSetup())
 
 	/*Create a dummy Instance */
 	uid, _ := id.NewUUID()
 	uid_str := uid.String()
-	err = etcdConn.Set(uid_str, "How are you")
+	err = types.Gdb.Set(uid_str, "How are you")
 
 	if err != nil {
-		fmt.Println("Error setting value ", err)
+		log.Println("Error setting value ", err)
 	}
 
-	value, err := etcdConn.Get(uid_str)
-	fmt.Printf("GET err:%v, value:%v\n", err, value)
+	value, err := types.Gdb.Get(uid_str)
+	log.Printf("GET err:%v, value:%v\n", err, value)
+
+	/* Use the instance library */
+	I := types.NewInstance("TestInstance", "S", 1, 0)
+	I.Status = "Innactive"
+
+	/* Write the entier data to db store */
+	if !I.Sync() {
+		log.Printf("Failed to write the structure to etcd")
+	}
+
+	J := &types.Instance{}
+
+	J.Name = "TestInstance"
+
+	if !J.Load() {
+		log.Printf("Failed to read the structure from the store")
+	}
+
+	/* Print the contents of the structure read fromt he store */
+	log.Printf("Value read from store {Name : %s, Type : %s, Masters : %d, Slaves : %d, Status : %s}", J.Name, J.Type, J.Masters, J.Slaves, J.Status)
 
 }
