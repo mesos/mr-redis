e7387ecae44018c6448bffa5f8f34b76f2ec13a4
Anvith KS @ Huawei <anvith.ks@huawei.com>
2016-05-30 13:24:51 +0530

    Changed the directory structure to match the installable app directory structure. Added the package.json, bower.json and gruntfile.js files.

---
+++ ui/test/karma.conf.js
@@ -0,0 +1,95 @@
+// Karma configuration
+// Generated on 2016-05-03
+
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      'jasmine'
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // bower:js
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/underscore/underscore.js',
+      'bower_components/angular/angular.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-material-sidenav/angular-material-sidenav.js',
+      'bower_components/angular-material-icons/angular-material-icons.min.js',
+      'bower_components/angular-material-data-table/dist/md-data-table.js',
+      'bower_components/angular-spinners/dist/angular-spinners.min.js',
+      'bower_components/svg-morpheus/compile/minified/svg-morpheus.js',
+      'bower_components/angular-translate/angular-translate.js',
+      'bower_components/highcharts-ng/dist/highcharts-ng.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      // endbower
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // web server port
+    port: 8080,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      'Chrome',
+      'PhantomJS'
+    ],
+
+    // Which plugins to enable
+    plugins: [
+      /*'karma-phantomjs-launcher',*/
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+  });
+};
