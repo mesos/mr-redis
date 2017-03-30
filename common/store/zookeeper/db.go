a2fd595b2d8598fb64081282b485cae98ee94da1
Ubuntu <andrew@tractable.io>
2017-03-30 18:57:12 +0000

    making trailing slash removal consistent

--- common/store/zookeeper/db.go
+++ common/store/zookeeper/db.go
@@ -141,6 +141,7 @@ func (db *zkDB) DeleteSection(Key string) error {
 
 //ListSection will list a directory
 func (db *zkDB) ListSection(Key string, Recursive bool) ([]string, error) {
-	result, _, err := db.Con.Children(Key.TrimRight("/"))
+	Key = strings.TrimSuffix(Key, "/")
+	result, _, err := db.Con.Children(Key)
 	return result, err
 }
