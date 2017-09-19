f7dfc6bd6082fc1a48b8297bea2c98f3de57d208
zou sheng <zousheng@easemob.com>
2017-09-19 22:32:44 +0800

    Change default log module to github.com/hhkbp2/go-logging for more specific logs, and remove clean_proxy_port func to avoid concurrent read and write map

--- proxy/redis_proxy.go
+++ proxy/redis_proxy.go
@@ -4,10 +4,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 	"github.com/curator-go/curator"
-	"github.com/natefinch/lumberjack"
 	"github.com/samuel/go-zookeeper/zk"
+	"github.com/hhkbp2/go-logging"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -33,7 +32,7 @@ const (
 	//RedisPortBaseNum Local redis listen port range from 6100
 	RedisPortMinNum = 6100
 
-	RedisPortMaxNum = 6300
+	RedisPortMaxNum = 6400
 
 	ProxyPort = 7979
 
@@ -48,6 +47,14 @@ const (
 	RedisPath = "/MrRedis/Instances"
 
 	RedisLocalPortsPath = "/MrRedisLocalPorts"
+
+	LogFilePath = "/data/apps/log/MrRedis-local-proxy.log"
+
+	LogFileMaxSize = 100 * 1024 * 1024   // megabytes
+
+	LogFileMaxBackups = 10
+
+	LogFileMaxAge = 7    //days
 
 )
 
@@ -77,7 +84,7 @@ type HTTPUpdate struct {
 
 func must(err error) {
 	if err != nil {
-		log.Println("panic")
+		logger.Error("panic")
 		panic(err)
 	}
 }
@@ -105,6 +112,11 @@ func newTCPListener(addr string) (net.Listener, error) {
 }
 */
 
+
+var logger = logging.GetLogger("redis_proxy")
+
+
+
 func RandInt64(min, max int) int {
 	if min >= max || min == 0 || max == 0 {
 		return max
@@ -113,19 +125,19 @@ func RandInt64(min, max int) int {
 }
 
 func PrepareLocalPorts(conn *zk.Conn, path string) {
-	log.Printf("Begin to prepare redis_local_ports")
+	logger.Info("Begin to prepare redis_local_ports")
 	found, _, err := conn.Exists(path)
 	must(err)
 	if found {
-		log.Println(path + " already exist.")
+		logger.Infof(path + " already exist.")
 	} else {
-		log.Println(path + " doesn't exist, need to create it.")
+		logger.Infof(path + " doesn't exist, need to create it.")
 		flags := int32(0)
 		acl := zk.WorldACL(zk.PermAll)
 
 		_, err := conn.Create(path, []byte("Mesos_local_ports_parent"), flags, acl)
 		if err != nil {
-			log.Println("Failed to create parent node " + path)
+			logger.Warnf("Failed to create parent node " + path)
 		}
 	}
 
@@ -140,35 +152,31 @@ func PrepareLocalPorts(conn *zk.Conn, path string) {
 		_, ok := LocalPortsMap[name]
 
 		if ok {
-			log.Printf("%s local port %s already exist in LocalPortsMap.\n", name, local_port)
+			logger.Infof("%s local port %s already exist in LocalPortsMap.\n", name, local_port)
 		} else {
 			LocalPortsMap[name] = string(local_port)
 		}
 
 	}
 
-	log.Println("LocalPortsMap is")
-	log.Println(LocalPortsMap)
-
 }
 
 func DeleteZKPathRecursive(path string) {
+
 	zksStr := os.Getenv("ZOOKEEPER_SERVERS")
-	//zks := strings.Split(zksStr, ",")
 
 	if zksStr != "" {
-
 		retryPolicy := curator.NewExponentialBackoffRetry(time.Second, CleanUpZKMaxReties, CleanUpZKCheckIntervalSecs*time.Second)
 		client := curator.NewClient(zksStr, retryPolicy)
 		client.Start()
 		client.Delete().DeletingChildrenIfNeeded().ForPath(path)
-		log.Printf("deleteZKPathRecursive: remove zk znode %s recursively.", path)
+		logger.Infof("deleteZKPathRecursive: remove zk znode %s recursively.", path)
 
 		defer client.Close()
 
 	} else {
 
-		log.Printf("deleteZKPathRecursive: failed to get env variable ZOOKEEPER_SERVERS.")
+		logger.Error("deleteZKPathRecursive: failed to get env variable ZOOKEEPER_SERVERS.")
 
 	}
 }
@@ -178,11 +186,10 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 	redis_instance, _, err := conn.Children(path)
 
-	must(err)
-
-	//time.Sleep(time.Second * 15)
-
-	log.Println("InitializeProxy: Begin to initialize ans sync proxy servers from zk.")
+	if err != nil {
+		logger.Error("Failed to load all redis instances from zk mr-redis path /MrRedis/Instances .")
+		panic(err)
+	}
 
 	for _, name := range redis_instance {
 
@@ -191,9 +198,7 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 		if redis_status != nil && strings.EqualFold(string(redis_status), "RUNNING") {
 
-			// var CurrentE Entry
-
-			//var CurrentE Entry
+			logger.Infof("redis instance %s status is running.", name)
 
 			redis_id, _, err := conn.Get(RedisPath + "/" + name + "/Mname")
 
@@ -211,7 +216,7 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 			if CurrentE, ok := ConfigMap[name]; ok {
 
-				log.Printf("InitializeProxy: Redis name %s is already in the configMap, only change its backend redis addr. \n", name)
+				logger.Infof("Redis instance %s is in the configMap. \n", name)
 
 				if found, _, _ := conn.Exists(RedisLocalPortsPath + "/" + name); found {
 
@@ -219,12 +224,12 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 					redis_tcp_local_port = string(redis_port_byte[:])
 
-					log.Printf("InitializeProxy: Redis local port %s is already in the MrRedisLocalPort, sync with zk to keep it consistent . \n", redis_tcp_local_port)
+					logger.Infof("InitializeProxy: Redis %s local port %s is already in the MrRedisLocalPort, sync with zk to keep it consistent . \n", name, redis_tcp_local_port)
 
 					CurrentE.Pair.From = "127.0.0.1" + ":" + redis_tcp_local_port
 
-					log.Printf("InitializeProxy: set redis instance %s Pair.From properties to %s" , name, CurrentE.Pair.From)
-					
+					logger.Infof("Set redis instance %s Pair.From properties to %s" , name, CurrentE.Pair.From)
+
 				}
 
 				CurrentE.Pair.To = string(redis_ip) + ":" + string(redis_port)
@@ -233,7 +238,7 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 			} else {
 
-				log.Printf("InitializeProxy: Redis name %s not found in the configMap \n", name)
+				logger.Infof("Redis name %s not found in the configMap \n", name)
 
 				if found, _, _ := conn.Exists(RedisLocalPortsPath + "/" + name); found {
 
@@ -241,7 +246,7 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 					redis_tcp_local_port = string(redis_port_byte[:])
 
-					log.Printf("redis instance %s exist, redis tcp local port is %s \n", name, redis_tcp_local_port)
+					logger.Infof("redis instance %s already exists, redis tcp local port is %s \n", name, redis_tcp_local_port)
 
 				} else {
 
@@ -254,34 +259,32 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 						redis_tcp_local_port = strconv.Itoa(random_port)
 
-						log.Printf("redis %s generate random local_ ort number is %s \n", name, redis_tcp_local_port)
+						logger.Infof("redis %s generate random local_ ort number is %s \n", name, redis_tcp_local_port)
 
 						local_port_num := len(LocalPortsMap)
 
-						log.Printf("local port num is %d \n", local_port_num)
+						logger.Infof("redis %s local port num is %d \n", name, local_port_num)
 
 						if local_port_num > 0 {
 							for _, value := range LocalPortsMap {
 								if strings.EqualFold(redis_tcp_local_port, value) {
 									redis_port_found = true
-									log.Printf("Redis %sredis port %s is already assigned.\n", name, value)
+									logger.Infof("Redis %sredis port %s is already assigned.\n", name, value)
 									break
 								}
 							}
 
 							if redis_port_found {
-								log.Printf("Local tcp port %s is duplicated, will generate a new one.\n", redis_tcp_local_port)
+								logger.Infof("Local tcp port %s is duplicated, will generate a new one.\n", redis_tcp_local_port)
 								continue
 							} else {
-								log.Printf("random_tcp_port not assigned in local, so it can be used, will skip this loop.")
+								logger.Info("random_tcp_port not assigned in local, so it can be used, will skip this loop.")
 								break
 							}
 						} else {
-							log.Println("LocalPortsMap length is zero, so a random port can be choosen")
+							logger.Warn("LocalPortsMap length is zero, so a random port can be choosen")
 							break
 						}
-
-						log.Printf("loop redis %s to check local port over\n", name)
 
 					}
 
@@ -302,7 +305,7 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 				to_tcp_addr, _ := net.ResolveTCPAddr("tcp4", to_addr)
 
-				log.Printf("InitializeProxy: Redis %s local_tcp_addr is %s, to_tcp_addr is %s \n", name, local_tcp_addr.String(), to_tcp_addr.String())
+				logger.Infof("Redis %s local_tcp_addr is %s, to_tcp_addr is %s \n", name, local_tcp_addr.String(), to_tcp_addr.String())
 
 				currentProxyPair := PorxyPair{From: local_tcp_addr.String(), To: to_tcp_addr.String()}
 
@@ -312,7 +315,6 @@ func InitializeProxy(conn *zk.Conn, path string) {
 
 				go HandleConnection(CurrentEntry)
 
-				log.Println("InitializeProxy: End of InitializeProxy")
 			}
 		}
 	}
@@ -326,12 +328,12 @@ func HandleConnection(E Entry) error {
 	var CurrentE Entry //A Temp variable to get the latest Desination proxy value
 	var OK bool
 
-	log.Printf("HandleConnection() %v", E)
+	logger.Info("HandleConnection() %v", E)
 	//src, err := net.Listen("tcp", E.Pair.From)
 	listener, err := newTCPListener(E.Pair.From)
 
 	if err != nil {
-		log.Printf("Error binding to the IP %v", err)
+		logger.Errorf("Error binding to the IP %v", err)
 		return err
 	}
 
@@ -343,13 +345,13 @@ func HandleConnection(E Entry) error {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Error accepting a new connection %v", err)
+			logger.Errorf("Error accepting a new connection %v", err)
 			continue
 		}
 
 		//Get the latest Entry from the MAP because it migh thave been updated on the fly.
 		if CurrentE, OK = ConfigMap[E.Name]; !OK {
-			log.Printf("Error Proxy entry is incorrect / empty for %s", E.Name)
+			logger.Errorf("Error Proxy entry is incorrect / empty for %s", E.Name)
 			conn.Close()
 			continue
 		}
@@ -362,7 +364,7 @@ func HandleConnection(E Entry) error {
 
 			T, err := net.Dial("tcp", E.Pair.To)
 			if err != nil {
-				log.Printf("Unable to connect to the Destination %s %v", E.Pair.To, err)
+				logger.Errorf("Unable to connect to the Destination %s %v", E.Pair.To, err)
 				return
 			}
 			defer T.Close()
@@ -382,25 +384,30 @@ func cleanProxy(conn *zk.Conn) {
 
 			time.Sleep(time.Second * CleanUpInterval)
 
-			log.Printf("cleanProxy: Sleep %d seconds", CleanUpInterval)
+			logger.Infof("cleanProxy: Sleep %d seconds", CleanUpInterval)
 
 			redis_instances, _, err := conn.Children(RedisPath)
 
-			log.Printf("cleanProxy: redis_instaces nodes are %v", redis_instances)
+			if err != nil {
+				logger.Errorf("Failed to get redis instances.")
+				return
+			}
+
+			logger.Infof("cleanProxy: redis_instaces nodes are %v", redis_instances)
 
 			for _,name := range redis_instances {
 
 				redis_status,_, err := conn.Get(RedisPath + "/" + name + "/" + "Status")
 
-				log.Printf("cleanProxy: redis %s status is %s", name, redis_status)
+				logger.Infof("cleanProxy: redis %s status is %s", name, redis_status)
 
 				if err != nil {
-					log.Printf("cleanProxy: err occured when getting redis %s path %v", name, err)
+					logger.Errorf("cleanProxy: err occured when getting redis %s path %v", name, err)
 					return
 				}
 
 
-				log.Printf("cleanProxy:redis %s status is %s.\n", name, redis_status)
+				logger.Infof("cleanProxy:redis %s status is %s.\n", name, redis_status)
 
 				if strings.EqualFold(string(redis_status), "DELETED") || redis_status == nil {
 
@@ -412,24 +419,22 @@ func cleanProxy(conn *zk.Conn) {
 					CurrentE, ok := ConfigMap[name]
 
 					if ok {
-						log.Printf("cleanProxy: redis %s is in the ConfigMap", name)
+						logger.Infof("cleanProxy: redis %s is in the ConfigMap", name)
 
 						from_addr := CurrentE.Pair.From
 
-						log.Printf("cleanProxy: redis %s from_addr is %s", name, from_addr)
+						logger.Infof("cleanProxy: redis %s from_addr is %s", name, from_addr)
 
 						delete(ConfigMap, name)
 
 					} else {
 
-						log.Printf("cleanProxy: redis %s is not in the ConfigMap", name)
+						logger.Infof("cleanProxy: redis %s is not in the ConfigMap", name)
 					}
 
 				}
 			}
-			if err != nil {
-				return
-			}
+
 		}
 	}()
 }
@@ -444,7 +449,7 @@ func HandleHTTPUpdate(w http.ResponseWriter, r *http.Request) {
 		r.Body.Close()
 		if err != nil {
 			//log.Printf(w, "Error understanding the Body %v", err)
-			log.Printf("Error understanding the Body %v", err)
+			logger.Errorf("Error understanding the Body %v", err)
 			return
 		}
 
@@ -454,15 +459,15 @@ func HandleHTTPUpdate(w http.ResponseWriter, r *http.Request) {
 		err = json.Unmarshal(content, &val)
 		if err != nil {
 			//log.Printf(w, "Wrong json format %v", err)
-			log.Printf("Wrong json format %v", err)
+			logger.Errorf("Wrong json format %v", err)
 			return
 		}
 		if CurrentE, OK = ConfigMap[val.Name]; !OK {
-			log.Printf("Error Proxy entry is incorrect / empty for %s", val.Name)
+			logger.Infof("Error Proxy entry is incorrect / empty for %s", val.Name)
 			//log.Printf(w, "Error Proxy entry is incorrect / empty for %s", val.Name)
 			return
 		}
-		log.Printf("Updating From porxy for %s From %s TO %s", val.Name, CurrentE.Pair.To, val.Addr)
+		logger.Info("Updating From porxy for %s From %s TO %s", val.Name, CurrentE.Pair.To, val.Addr)
 		CurrentE.Pair.To = val.Addr
 		ConfigMap[val.Name] = CurrentE
 		return
@@ -474,7 +479,7 @@ func HandleHTTPUpdate(w http.ResponseWriter, r *http.Request) {
 func HandleHTTPGet(w http.ResponseWriter, r *http.Request) {
 	retBytes, err := json.MarshalIndent(ConfigMap, " ", "  ")
 	if err != nil {
-		log.Printf("Error Marshalling HandleHTTPGet() %v", err)
+		logger.Errorf("Error Marshalling HandleHTTPGet() %v", err)
 		//log.Printf(w, "Error Marshalling HandleHTTPGet() %v", err)
 		return
 
@@ -493,31 +498,66 @@ func main() {
 	//Initialize the global LocalPorts map
 	LocalPortsMap = make(map[string]string)
 
-	//set log rotating policy
-
-	log.SetOutput(&lumberjack.Logger{
-		Filename:   "/data/apps/log/MrRedis-local-proxy.log",
-		MaxSize:    50, // megabytes
-		MaxBackups: 10,
-		MaxAge:     3, //days
-	})
-
 	//Read a config file that has json update the config files
 	cfgFileName := flag.String("config", "./config.json", "Supply the location of MrRedis configuration file")
 	flag.Parse()
 
-	log.Printf("The config file name is %s ", *cfgFileName)
+	filePath := LogFilePath
+
+	fileMode := os.O_APPEND
+
+	bufferSize := 0
+
+	bufferFlushTime := 30 * time.Second
+
+	inputChanSize := 1
+
+	backupCount := uint32(LogFileMaxBackups)
+	// set the maximum size of every file to 100 M bytes
+	fileMaxBytes := uint64(LogFileMaxSize)
+
+
+
+	// create a handler(which represents a log message destination)
+	handler := logging.MustNewRotatingFileHandler(
+		filePath, fileMode, bufferSize, bufferFlushTime, inputChanSize,
+		fileMaxBytes, backupCount)
+
+
+	// the format for the whole log message
+	format := "%(asctime)s %(levelname)s (%(filename)s:%(lineno)d) " +
+		"%(name)s %(message)s"
+
+	// the format for the time part
+	dateFormat := "%Y-%m-%d %H:%M:%S.%3n"
+
+	// create a formatter(which controls how log messages are formatted)
+	formatter := logging.NewStandardFormatter(format, dateFormat)
+
+	// set formatter for handler
+	handler.SetFormatter(formatter)
+
+
+	logger.SetLevel(logging.LevelInfo)
+
+	logger.AddHandler(handler)
+	
+
+	// ensure all log messages are flushed to disk before program exits.
+	defer logging.Shutdown()
+
+	logger.Infof("The config file name is %s ", *cfgFileName)
 	cfgFile, err := ioutil.ReadFile(*cfgFileName)
 
 	if err != nil {
-		log.Printf("Error Reading the configration file. Resorting to default values")
+		logger.Error("Error Reading the configration file. Resorting to default values")
 	}
 	err = json.Unmarshal(cfgFile, &Cfg)
 	if err != nil {
-		log.Fatalf("Error parsing the config file %v", err)
+		logger.Errorf("Error parsing the config file %v", err)
 		return
 	}
-	log.Printf("Configuration file is = %v", Cfg)
+	logger.Infof("Configuration file is = %v", Cfg)
 
 	conn := connect()
 
@@ -532,12 +572,8 @@ func main() {
 	InitializeProxy(conn, RedisPath)
 
 	//Clean up unused tcp ports, eg: when redis status is DELETED, the local proxy server of that redis will be shutdown.
-
-	conn1 := connect()
-
-	defer conn1.Close()
-
-	cleanProxy(conn1)
+	//comment cleanProxy to avoid concurrent write Map error
+	//cleanProxy(conn)
 
 	go func() {
 
@@ -545,7 +581,7 @@ func main() {
 
 			time.Sleep(time.Second * SyncZKIntervalSecs)
 
-			log.Printf("Routine: Sync redis infomration from zk...")
+			logger.Info("Routine: Sync redis infomration from zk...")
 
 			InitializeProxy(conn, RedisPath)
 		}
@@ -556,7 +592,7 @@ func main() {
 
 	http.HandleFunc("/Get/", HandleHTTPGet)
 
-	log.Fatal(http.ListenAndServe(":"+Cfg.HTTPPort, nil))
+	logger.Fatal(http.ListenAndServe(":"+Cfg.HTTPPort, nil))
 
 	//Wait indefinitely
 	waitCh := make(chan bool)
@@ -564,4 +600,3 @@ func main() {
 	<-waitCh
 
 }
-
